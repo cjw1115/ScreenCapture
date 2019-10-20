1154efc4a70159b5e4a40a05c9671078ab5ca1b6
陈佳卫 <cjw1115@hotmail.com>
2019-10-20 14:14:52 +0800

    Save bitmap to disk, then composite them with MediaComposition

---
+++ ScreenCapture/ImageHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace ScreenCapture
+{
+    public class ImageHelper
+    {
+        public static async Task SaveSoftwareBitmapToFileAsync(SoftwareBitmap softwareBitmap, StorageFile outputFile)
+        {
+            using (IRandomAccessStream stream = await outputFile.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                // Create an encoder with the desired format
+                BitmapEncoder encoder = await BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+
+                // Set the software bitmap
+                encoder.SetSoftwareBitmap(softwareBitmap);
+                encoder.IsThumbnailGenerated = true;
+
+                try
+                {
+                    await encoder.FlushAsync();
+                }
+                catch (Exception err)
+                {
+                    const int WINCODEC_ERR_UNSUPPORTEDOPERATION = unchecked((int)0x88982F81);
+                    switch (err.HResult)
+                    {
+                        case WINCODEC_ERR_UNSUPPORTEDOPERATION:
+                            // If the encoder does not support writing a thumbnail, then try again
+                            // but disable thumbnail generation.
+                            encoder.IsThumbnailGenerated = false;
+                            break;
+                        default:
+                            throw;
+                    }
+                }
+
+                if (encoder.IsThumbnailGenerated == false)
+                {
+                    await encoder.FlushAsync();
+                }
+            }
+        }
+    }
+}
--- ScreenCapture/MainPage.xaml.cs
+++ ScreenCapture/MainPage.xaml.cs
@@ -1,80 +1,107 @@
-﻿using Microsoft.Graphics.Canvas;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using Windows.Foundation;
-using Windows.Graphics;
 using Windows.Graphics.Capture;
+using Windows.Graphics.DirectX;
+using Windows.Graphics.Imaging;
 using Windows.Media.Editing;
 using Windows.Media.Transcoding;
-using Windows.UI.Composition;
-using Windows.UI.Core;
+using Windows.Storage;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using System.Collections.Concurrent;
+using Microsoft.Graphics.Canvas;
 
 namespace ScreenCapture
 {
-
-
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class MainPage : Page
     {
-        // Non-API related members.
-        private ScreenCaptureComponent.CaptureHelper _captureHelper = new ScreenCaptureComponent.CaptureHelper();
-        private MediaComposition _mediaComposition = new MediaComposition();
+        
         public MainPage()
         {
             InitializeComponent();
         }
 
-        public async Task StartCaptureAsync()
+        private async Task _startCaptureAsync()
         {
-            // The GraphicsCapturePicker follows the same pattern the
-            // file pickers do.
             var picker = new GraphicsCapturePicker();
             GraphicsCaptureItem item = await picker.PickSingleItemAsync();
 
-            // The item may be null if the user dismissed the
-            // control without making a selection or hit Cancel.
             if (item != null)
             {
-                StartCaptureInternal(item);
-                //_captureHelper.StartCaptureInternal(TimeSpan.FromMilliseconds(20), _mediaComposition, item);
+                _captureFolder = await _setupCpatureFolder();
+                _startCaptureInternal(item);
+
+                Task.Run(() =>
+                {
+                    _renderCaptuedImagesToFiles();
+                });
             }
         }
 
-        private async void Button_ClickAsync(object sender, RoutedEventArgs e)
+        private async void _btnStartClickAsync(object sender, RoutedEventArgs e)
         {
-            await StartCaptureAsync();
+            await _startCaptureAsync();
         }
 
-        private async void Save_Click(object sender, RoutedEventArgs e)
+        private void _btnStopClick(object sender, RoutedEventArgs e)
+        {
+            _seesion.Dispose();
+            _framePool.Dispose();
+        }
+
+        private async void _btnSaveClick(object sender, RoutedEventArgs e)
         {
             try
             {
-                await RenderCompositionToFile(_mediaComposition);
+                while(_captuedImages.Count>0)
+                {
+                    await Task.Delay(20);
+                }
+                await _setupMediaComposition();
+                await _renderCompositionToFile();
             }
 #pragma warning disable CS0168 // Variable is declared but never used
             catch (Exception)
 #pragma warning restore CS0168 // Variable is declared but never used
             {
-
             }
-            
+
         }
 
-        private void Stop_Click(object sender, RoutedEventArgs e)
+        private async Task _renderCaptuedImagesToFiles()
         {
-            _seesion.Dispose();
-            _framePool.Dispose();
-
-            //_captureHelper.Stop();
+            while(true)
+            {
+                bool re = false;
+                do
+                {
+                    re = _captuedImages.TryDequeue(out SoftwareBitmap bitmap);
+                    if (re)
+                    {
+                        var file = await _captureFolder.CreateFileAsync($"Screenshot_{++_imageCounter}.jpeg");
+                        await ImageHelper.SaveSoftwareBitmapToFileAsync(bitmap, file);
+                        bitmap.Dispose();
+                    }
+                }
+                while (re);
+                await Task.Delay(20);
+            }
         }
 
-        private async Task RenderCompositionToFile(MediaComposition composition)
+        private MediaComposition _mediaComposition;
+        private async Task _setupMediaComposition()
+        {
+            var _captureImageFiles =await _captureFolder.GetFilesAsync();
+            _mediaComposition = new MediaComposition();
+            foreach (var imageFile in _captureImageFiles)
+            {
+                MediaClip clip = await MediaClip.CreateFromImageFileAsync(imageFile,TimeSpan.FromMilliseconds(20));
+                _mediaComposition.Clips.Add(clip);
+            }
+        }
+        private async Task _renderCompositionToFile()
         {
             var picker = new Windows.Storage.Pickers.FileSavePicker();
             picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
@@ -85,37 +112,29 @@ namespace ScreenCapture
             if (file != null)
             {
                 // Call RenderToFileAsync
-                var saveOperation = composition.RenderToFileAsync(file, MediaTrimmingPreference.Precise);
+                var saveOperation = _mediaComposition.RenderToFileAsync(file, MediaTrimmingPreference.Precise);
 
-                saveOperation.Progress = new AsyncOperationProgressHandler<TranscodeFailureReason, double>(async (info, progress) =>
+                saveOperation.Progress = new AsyncOperationProgressHandler<TranscodeFailureReason, double>((info, progress) =>
                 {
-                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(() =>
-                    {
-                        System.Diagnostics.Debug.WriteLine(string.Format("Saving file... Progress: {0:F0}%", progress));
-                    }));
+                    System.Diagnostics.Debug.WriteLine(string.Format("Saving file... Progress: {0:F0}%", progress));
                 });
-                saveOperation.Completed = new AsyncOperationWithProgressCompletedHandler<TranscodeFailureReason, double>(async (info, status) =>
+                saveOperation.Completed = new AsyncOperationWithProgressCompletedHandler<TranscodeFailureReason, double>((info, status) =>
                 {
-                    await this.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(() =>
+                    try
                     {
-                        try
+                        var results = info.GetResults();
+                        if (results != TranscodeFailureReason.None || status != AsyncStatus.Completed)
                         {
-                            var results = info.GetResults();
-                            if (results != TranscodeFailureReason.None || status != AsyncStatus.Completed)
-                            {
-                                System.Diagnostics.Debug.WriteLine(("Saving was unsuccessful"));
-                            }
-                            else
-                            {
-                                System.Diagnostics.Debug.WriteLine("Trimmed clip saved to file");
-                            }
+                            System.Diagnostics.Debug.WriteLine(("Saving was unsuccessful"));
                         }
-                        finally
+                        else
                         {
-                            // Update UI whether the operation succeeded or not
+                            System.Diagnostics.Debug.WriteLine("Trimmed clip saved to file");
                         }
-
-                    }));
+                    }
+                    finally
+                    {
+                    }
                 });
             }
             else
@@ -127,59 +146,47 @@ namespace ScreenCapture
 
         Direct3D11CaptureFramePool _framePool;
         GraphicsCaptureSession _seesion;
-        CanvasDevice CanvasDevice = new CanvasDevice();
-        void StartCaptureInternal(GraphicsCaptureItem item)
-        {  
-            _framePool = Direct3D11CaptureFramePool.Create(CanvasDevice, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized, 2, item.Size);
+
+        private readonly string CAPTURE_FOLDER = "CaptureFolder";
+        private Windows.Storage.StorageFolder _captureFolder = null;
+        async Task<StorageFolder> _setupCpatureFolder()
+        {
+            StorageFolder storageFolder = null;
+
+            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.GetFolderAsync(CAPTURE_FOLDER);
+            if (storageFolder != null)
+            {
+                await storageFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            }
+            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.CreateFolderAsync(CAPTURE_FOLDER);
+
+            return storageFolder;
+        }
+
+        private List<StorageFile> _captureImageFiles;
+        private ConcurrentQueue<SoftwareBitmap> _captuedImages;
+        private readonly object _lockObejct = new object();
+        private uint _imageCounter = 0;
+        private ConcurrentQueue<SoftwareBitmap> _iamgeQueues;
+        private void _startCaptureInternal(GraphicsCaptureItem item)
+        {
+            _captureImageFiles = new List<StorageFile>();
+            _captuedImages = new ConcurrentQueue<SoftwareBitmap>();
+            _iamgeQueues = new ConcurrentQueue<SoftwareBitmap>();
+
+            _framePool = Direct3D11CaptureFramePool.Create(CanvasDevice.GetSharedDevice(), DirectXPixelFormat.B8G8R8A8UIntNormalized, 2, item.Size);
             _framePool.FrameArrived += async (o, e) => 
             {
                 using (var frame = _framePool.TryGetNextFrame())
                 {
-                    var softwareBitmap = await Windows.Graphics.Imaging.SoftwareBitmap.CreateCopyFromSurfaceAsync(frame.Surface);
-                    ProcessFrame(softwareBitmap);
+                    var softwareBitmap = await Windows.Graphics.Imaging.SoftwareBitmap.CreateCopyFromSurfaceAsync(frame.Surface, Windows.Graphics.Imaging.BitmapAlphaMode.Premultiplied);
+                    _captuedImages.Enqueue(softwareBitmap);
                 }
             };
             _seesion = _framePool.CreateCaptureSession(item);
             _seesion.StartCapture();
         }
 
-        private void ProcessFrame(Windows.Graphics.Imaging.SoftwareBitmap softwareBitmap)
-        {
-            CanvasRenderTarget rendertarget;
-            var pixelBuffer = _getPixelBuffer(softwareBitmap);
-            using (CanvasBitmap canvas = CanvasBitmap.CreateFromBytes(CanvasDevice.GetSharedDevice(true), pixelBuffer.ToArray(), softwareBitmap.PixelWidth, softwareBitmap.PixelHeight, Windows.Graphics.DirectX.DirectXPixelFormat.B8G8R8A8UIntNormalized))
-            {
-                rendertarget = new CanvasRenderTarget(CanvasDevice.GetSharedDevice(true), canvas.SizeInPixels.Width, canvas.SizeInPixels.Height, 96);
-                using (CanvasDrawingSession ds = rendertarget.CreateDrawingSession())
-                {
-                    ds.Clear(Windows.UI.Colors.Black);
-                    ds.DrawImage(canvas);
-                }
-            }
-            MediaClip clip = MediaClip.CreateFromSurface(rendertarget, TimeSpan.FromMilliseconds(20));
-            _mediaComposition.Clips.Add(clip);
-        }
 
-        public unsafe System.Span<byte> _getPixelBuffer(Windows.Graphics.Imaging.SoftwareBitmap softwareBitmap)
-        {
-            uint capacity;
-            using (Windows.Graphics.Imaging.BitmapBuffer buffer = softwareBitmap.LockBuffer(Windows.Graphics.Imaging.BitmapBufferAccessMode.Write))
-            {
-                using (var reference = buffer.CreateReference())
-                {
-                    byte* dataInBytes;
-
-                    ((IMemoryBufferByteAccess)reference).GetBuffer(out dataInBytes, out capacity);
-                    return new Span<byte>(dataInBytes, (int)capacity);
-                }
-            }
-        }
-        [ComImport]
-        [Guid("5B0D3235-4DBA-4D44-865E-8F1D0E4FD04D")]
-        [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
-        unsafe interface IMemoryBufferByteAccess
-        {
-            void GetBuffer(out byte* buffer, out uint capacity);
-        }
     }
 }
