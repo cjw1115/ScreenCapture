a58eca02b0a27f829a2026a0af3db84f66f2166e
陈佳卫 <cjw1115@hotmail.com>
2019-10-21 00:48:49 +0800

    Add thread to save the screenshot image to disk; Ingnore some frames from the frame pool.

--- ScreenCapture/MainPage.xaml.cs
+++ ScreenCapture/MainPage.xaml.cs
@@ -12,6 +12,10 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using System.Collections.Concurrent;
 using Microsoft.Graphics.Canvas;
+using System.Runtime.InteropServices;
+using Windows.Media.MediaProperties;
+using Windows.UI;
+using Windows.UI.Xaml.Media;
 
 namespace ScreenCapture
 {
@@ -22,6 +26,40 @@ namespace ScreenCapture
         {
             InitializeComponent();
         }
+
+        private readonly string CAPTURE_FOLDER = "CaptureFolder";
+
+        private MediaComposition _mediaComposition;
+        private Direct3D11CaptureFramePool _framePool;
+        private GraphicsCaptureSession _seesion;
+        private StorageFolder _captureFolder;
+        private async Task<StorageFolder> _setupCpatureFolder()
+        {
+            StorageFolder storageFolder = null;
+
+            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.GetFolderAsync(CAPTURE_FOLDER);
+            if (storageFolder != null)
+            {
+                await storageFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
+            }
+            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.CreateFolderAsync(CAPTURE_FOLDER);
+
+            return storageFolder;
+        }
+
+        private List<StorageFile> _capturedImageFiles;
+        private ConcurrentQueue<SoftwareBitmap> _capturedImages;
+        private ConcurrentQueue<TimeSpan> _captuedRelatedTimes;
+        private readonly object _lockObejct = new object();
+        private uint _imageCounter = 0;
+        private ConcurrentQueue<SoftwareBitmap> _iamgeQueues;
+
+        private uint filter = 0;
+
+        private TimeSpan _startTime;
+
+        [DllImport("Kernel32.dll")]
+        private static extern bool QueryPerformanceCounter(out long lpPerformanceCount);
 
         private async Task _startCaptureAsync()
         {
@@ -40,6 +78,125 @@ namespace ScreenCapture
             }
         }
 
+        private void _startCaptureInternal(GraphicsCaptureItem item)
+        {
+            _capturedImageFiles = new List<StorageFile>();
+            _capturedImages = new ConcurrentQueue<SoftwareBitmap>();
+            _iamgeQueues = new ConcurrentQueue<SoftwareBitmap>();
+            _captuedRelatedTimes = new ConcurrentQueue<TimeSpan>();
+
+            _framePool = Direct3D11CaptureFramePool.Create(CanvasDevice.GetSharedDevice(), DirectXPixelFormat.B8G8R8A8UIntNormalized, 2, item.Size);
+            _framePool.FrameArrived += async (o, e) =>
+            {
+
+                using (var frame = _framePool.TryGetNextFrame())
+                {
+                    if (filter++ % 2 == 0)
+                    {
+                        var softwareBitmap = await Windows.Graphics.Imaging.SoftwareBitmap.CreateCopyFromSurfaceAsync(frame.Surface, Windows.Graphics.Imaging.BitmapAlphaMode.Premultiplied);
+                        _capturedImages.Enqueue(softwareBitmap);
+                        _captuedRelatedTimes.Enqueue(frame.SystemRelativeTime);
+                    }
+                }
+            };
+            _seesion = _framePool.CreateCaptureSession(item);
+            _seesion.StartCapture();
+            QueryPerformanceCounter(out long counter);
+            _startTime = TimeSpan.FromTicks(counter);
+        }
+
+        private async Task _setupMediaComposition()
+        {
+            try
+            {
+                var _capturedImageFiles = await _captureFolder.GetFilesAsync();
+                _mediaComposition = new MediaComposition();
+                int i = 0;
+                var lastTime = _startTime;
+                foreach (var imageFile in _capturedImageFiles)
+                {
+                    if(_captuedRelatedTimes.TryDequeue(out TimeSpan relatedTime))
+                    {
+                        var duration = relatedTime - lastTime;
+                        lastTime = relatedTime;
+                        MediaClip clip = await MediaClip.CreateFromImageFileAsync(imageFile, duration);
+                        _mediaComposition.Clips.Add(clip);
+                    }
+                    
+                }
+            }
+            catch
+            {
+
+            }
+            
+        }
+
+
+        private async Task _renderCompositionToFile(StorageFile file)
+        {
+            if (file != null)
+            {
+                // Call RenderToFileAsync
+                var mp4Profile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.HD1080p);
+                var saveOperation = _mediaComposition.RenderToFileAsync(file, MediaTrimmingPreference.Precise, mp4Profile);
+
+                saveOperation.Progress = new AsyncOperationProgressHandler<TranscodeFailureReason, double>((info, progress) =>
+                {
+                    progressBar.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => 
+                    {
+                        progressBar.Value = progress;
+                    });
+                });
+                saveOperation.Completed = new AsyncOperationWithProgressCompletedHandler<TranscodeFailureReason, double>((info, status) =>
+                {
+                    try
+                    {
+                        var results = info.GetResults();
+                        if (results != TranscodeFailureReason.None || status != AsyncStatus.Completed)
+                        {
+                            System.Diagnostics.Debug.WriteLine(("Saving was unsuccessful"));
+                        }
+                        else
+                        {
+                            progressBar.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                            {
+                                progressBar.Foreground = new SolidColorBrush(Colors.Blue);
+                            });
+                            System.Diagnostics.Debug.WriteLine("Trimmed clip saved to file");
+                        }
+                    }
+                    finally
+                    {
+                    }
+                });
+            }
+            else
+            {
+                System.Diagnostics.Debug.WriteLine("User cancelled the file selection");
+            }
+        }
+
+        private async Task _renderCaptuedImagesToFiles()
+        {
+            while (true)
+            {
+                bool re = false;
+                do
+                {
+                    re = _capturedImages.TryDequeue(out SoftwareBitmap bitmap);
+                    if (re)
+                    {
+                        var file = await _captureFolder.CreateFileAsync($"Screenshot_{++_imageCounter}.jpeg");
+                        await ImageHelper.SaveSoftwareBitmapToFileAsync(bitmap, file);
+                        bitmap.Dispose();
+                    }
+                }
+                while (re);
+                await Task.Delay(20);
+            }
+        }
+
         private async void _btnStartClickAsync(object sender, RoutedEventArgs e)
         {
             await _startCaptureAsync();
@@ -55,138 +212,31 @@ namespace ScreenCapture
         {
             try
             {
-                while(_captuedImages.Count>0)
+                progressBar.Foreground = new SolidColorBrush(Colors.Green);
+
+                var picker = new Windows.Storage.Pickers.FileSavePicker();
+                picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
+                picker.FileTypeChoices.Add("MP4 files", new List<string>() { ".mp4" });
+                picker.SuggestedFileName = "RenderedComposition.mp4";
+
+                Windows.Storage.StorageFile file = await picker.PickSaveFileAsync();
+
+                while (_capturedImages.Count > 0)
                 {
                     await Task.Delay(20);
                 }
                 await _setupMediaComposition();
-                await _renderCompositionToFile();
+                await _renderCompositionToFile(file);
+
+                
             }
 #pragma warning disable CS0168 // Variable is declared but never used
             catch (Exception)
 #pragma warning restore CS0168 // Variable is declared but never used
             {
+                progressBar.Foreground = new SolidColorBrush(Colors.Red);
             }
 
         }
-
-        private async Task _renderCaptuedImagesToFiles()
-        {
-            while(true)
-            {
-                bool re = false;
-                do
-                {
-                    re = _captuedImages.TryDequeue(out SoftwareBitmap bitmap);
-                    if (re)
-                    {
-                        var file = await _captureFolder.CreateFileAsync($"Screenshot_{++_imageCounter}.jpeg");
-                        await ImageHelper.SaveSoftwareBitmapToFileAsync(bitmap, file);
-                        bitmap.Dispose();
-                    }
-                }
-                while (re);
-                await Task.Delay(20);
-            }
-        }
-
-        private MediaComposition _mediaComposition;
-        private async Task _setupMediaComposition()
-        {
-            var _captureImageFiles =await _captureFolder.GetFilesAsync();
-            _mediaComposition = new MediaComposition();
-            foreach (var imageFile in _captureImageFiles)
-            {
-                MediaClip clip = await MediaClip.CreateFromImageFileAsync(imageFile,TimeSpan.FromMilliseconds(20));
-                _mediaComposition.Clips.Add(clip);
-            }
-        }
-        private async Task _renderCompositionToFile()
-        {
-            var picker = new Windows.Storage.Pickers.FileSavePicker();
-            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.VideosLibrary;
-            picker.FileTypeChoices.Add("MP4 files", new List<string>() { ".mp4" });
-            picker.SuggestedFileName = "RenderedComposition.mp4";
-
-            Windows.Storage.StorageFile file = await picker.PickSaveFileAsync();
-            if (file != null)
-            {
-                // Call RenderToFileAsync
-                var saveOperation = _mediaComposition.RenderToFileAsync(file, MediaTrimmingPreference.Precise);
-
-                saveOperation.Progress = new AsyncOperationProgressHandler<TranscodeFailureReason, double>((info, progress) =>
-                {
-                    System.Diagnostics.Debug.WriteLine(string.Format("Saving file... Progress: {0:F0}%", progress));
-                });
-                saveOperation.Completed = new AsyncOperationWithProgressCompletedHandler<TranscodeFailureReason, double>((info, status) =>
-                {
-                    try
-                    {
-                        var results = info.GetResults();
-                        if (results != TranscodeFailureReason.None || status != AsyncStatus.Completed)
-                        {
-                            System.Diagnostics.Debug.WriteLine(("Saving was unsuccessful"));
-                        }
-                        else
-                        {
-                            System.Diagnostics.Debug.WriteLine("Trimmed clip saved to file");
-                        }
-                    }
-                    finally
-                    {
-                    }
-                });
-            }
-            else
-            {
-                System.Diagnostics.Debug.WriteLine("User cancelled the file selection");
-            }
-        }
-
-
-        Direct3D11CaptureFramePool _framePool;
-        GraphicsCaptureSession _seesion;
-
-        private readonly string CAPTURE_FOLDER = "CaptureFolder";
-        private Windows.Storage.StorageFolder _captureFolder = null;
-        async Task<StorageFolder> _setupCpatureFolder()
-        {
-            StorageFolder storageFolder = null;
-
-            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.GetFolderAsync(CAPTURE_FOLDER);
-            if (storageFolder != null)
-            {
-                await storageFolder.DeleteAsync(StorageDeleteOption.PermanentDelete);
-            }
-            storageFolder = await Windows.Storage.ApplicationData.Current.LocalCacheFolder.CreateFolderAsync(CAPTURE_FOLDER);
-
-            return storageFolder;
-        }
-
-        private List<StorageFile> _captureImageFiles;
-        private ConcurrentQueue<SoftwareBitmap> _captuedImages;
-        private readonly object _lockObejct = new object();
-        private uint _imageCounter = 0;
-        private ConcurrentQueue<SoftwareBitmap> _iamgeQueues;
-        private void _startCaptureInternal(GraphicsCaptureItem item)
-        {
-            _captureImageFiles = new List<StorageFile>();
-            _captuedImages = new ConcurrentQueue<SoftwareBitmap>();
-            _iamgeQueues = new ConcurrentQueue<SoftwareBitmap>();
-
-            _framePool = Direct3D11CaptureFramePool.Create(CanvasDevice.GetSharedDevice(), DirectXPixelFormat.B8G8R8A8UIntNormalized, 2, item.Size);
-            _framePool.FrameArrived += async (o, e) => 
-            {
-                using (var frame = _framePool.TryGetNextFrame())
-                {
-                    var softwareBitmap = await Windows.Graphics.Imaging.SoftwareBitmap.CreateCopyFromSurfaceAsync(frame.Surface, Windows.Graphics.Imaging.BitmapAlphaMode.Premultiplied);
-                    _captuedImages.Enqueue(softwareBitmap);
-                }
-            };
-            _seesion = _framePool.CreateCaptureSession(item);
-            _seesion.StartCapture();
-        }
-
-
     }
 }
