94b4722be71aeeae40308a0da77eb12c3fd16f7c
陈佳卫 <cjw1115@hotmail.com>
2019-11-30 21:31:53 +0800

    Remove Thumbnail in generated temp screenshot file.

--- ScreenCapture/Helper/ImageHelper.cs
+++ ScreenCapture/Helper/ImageHelper.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation;
 using Windows.Graphics.Imaging;
+using Windows.Media.Editing;
 using Windows.Storage;
 using Windows.Storage.Streams;
 
@@ -20,31 +22,8 @@ namespace ScreenCapture.Helper
 
                 // Set the software bitmap
                 encoder.SetSoftwareBitmap(softwareBitmap);
-                encoder.IsThumbnailGenerated = true;
-
-                try
-                {
-                    await encoder.FlushAsync();
-                }
-                catch (Exception err)
-                {
-                    const int WINCODEC_ERR_UNSUPPORTEDOPERATION = unchecked((int)0x88982F81);
-                    switch (err.HResult)
-                    {
-                        case WINCODEC_ERR_UNSUPPORTEDOPERATION:
-                            // If the encoder does not support writing a thumbnail, then try again
-                            // but disable thumbnail generation.
-                            encoder.IsThumbnailGenerated = false;
-                            break;
-                        default:
-                            throw;
-                    }
-                }
-
-                if (encoder.IsThumbnailGenerated == false)
-                {
-                    await encoder.FlushAsync();
-                }
+                encoder.IsThumbnailGenerated = false;
+                await encoder.FlushAsync();
             }
         }
     }
